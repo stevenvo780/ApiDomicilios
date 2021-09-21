f442fb9e6c09ca3c0b7e5e6ad75d967e93def50b
sebastian-crow <sebastianberiio@gmail.com>
2021-09-21 17:08:02 -0500

    Validation before save data already

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,12 +1,30 @@
+import Cors from 'cors'
+import initMiddleware from '../../lib/initMiddleware'
+import { v4 as uuidv4 } from 'uuid';
+
+const cors = initMiddleware(
+    Cors({
+        // Only allow request with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
+    })
+)
+
+
+
 const { connectToDatabase } = require('../../lib/mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 export default async function handler(req, res) {
+
+    await cors(req, res)
+
     // switch the methods
     switch (req.method) {
+
         case 'GET': {
             return getUsers(req, res);
         }
+
 
         case 'POST': {
             return addUser(req, res);
@@ -25,8 +43,12 @@ export default async function handler(req, res) {
 
 
 async function getUsers(req, res) { // Working
+
+    await cors(req, res)
+
     try {
         // connect to the database
+
         const { db } = await connectToDatabase()
 
         // Fetch the posts
@@ -53,28 +75,55 @@ async function getUsers(req, res) { // Working
 }
 
 
+
+
 async function addUser(req, res) { // Working
+
     try {
+        await cors(req, res)
         // Connect to the database
         let { db } = await connectToDatabase()
 
-        const { name, current_location, rol } = req.body
+        const { username, email, address, password, rol } = req.body
 
         const user = {
-            name: name,
-            current_location: current_location,
-            rol: rol
+            id: uuidv4(),
+            username,
+            email,
+            address,
+            password,
+            rol
         }
 
-        // Add the User    
-        //await db.collection("User").insertOne(JSON.parse(req.body))
-        await db.collection("User").insertOne(user)
+        let valEmail = await db.collection("User").findOne({ email: email})
 
-        // Return a message
+        if (valEmail) {
+            res.json({
+                message: 'User already exits',
+                success: false
+            })
+        } else {
+    
+            // Add the User    
+            //await db.collection("User").insertOne(JSON.parse(req.body))
+
+            await db.collection("User").insertOne(user)
+
+            // Return a message
+            
+        }
+
         return res.json({
             message: 'User Added Succesfully',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-type': 'application/json'
+            },
             success: true,
         })
+
+        
+
     } catch (e) {
         // Return an error
         return res.json({
@@ -85,15 +134,66 @@ async function addUser(req, res) { // Working
 }
 
 
+
+/*
+async function addUser(req, res) { // Working
+    try {
+        // Connect to the database
+        let { db } = await connectToDatabase()
+
+        const { name, address, password, rol } = req.body
+
+        const user = {
+            name,
+            address, 
+            password,            
+            rol
+        }
+
+        // Add the User    
+        //await db.collection("User").insertOne(JSON.parse(req.body))
+        await db.collection("User").insertOne(user)
+
+        // Return a message
+        return res.json({
+            message: 'User Added Succesfully',
+            Headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-type': 'application/json'
+            },
+            success: true,
+        })
+    } catch (e) {
+        // Return an error
+        return res.json({
+            message: new Error(e).message,
+            success: false,
+        })
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function updateUser(req, res) { // Working
     try {
         // Connect to the database
         let { db } = await connectToDatabase()
-      
+
 
         // Update the published status of the post
 
-        const { _id, name, current_location} = req.body
+        const { _id, name, current_location } = req.body
 
         const updateUser = {
             _id,
@@ -105,7 +205,7 @@ async function updateUser(req, res) { // Working
             {
                 _id: updateUser._id
             },
-            {$set: {published: true}}
+            { $set: { published: true } }
         )
 
 
